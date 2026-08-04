c9087cc4fc950024cf202dfbd513980dec1a03ca
AlinaRsy <al.alinarsy616@gmail.com>
2026-08-04 18:38:26 +0300

    refactoring hw5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -25,9 +25,9 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
           <NavLink
             id={"hw5-pre-junior-link"}
             to={PATH.PRE_JUNIOR}
-            // onClick={handleClose}
+            onClick={handleClose}
             className={({ isActive }) => 
-              isActive ? 'active' : s.nav__link  // ← Используем 'active'
+              isActive ? `${s.nav__link} ${s.active}` : s.nav__link
             }
           >
             Pre-junior
@@ -35,9 +35,9 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
           <NavLink
             id={"hw5-junior-link"}
             to={PATH.JUNIOR}
-            // onClick={handleClose}
+            onClick={handleClose}
             className={({ isActive }) => 
-              isActive ? 'active' : s.nav__link  // ← Используем 'active'
+              isActive ? `${s.nav__link} ${s.active}` : s.nav__link
             }
           >
             Junior
@@ -45,9 +45,9 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
           <NavLink
             id={"hw5-junior-plus-link"}
             to={PATH.JUNIOR_PLUS}
-            // onClick={handleClose}
+            onClick={handleClose}
             className={({ isActive }) => 
-              isActive ? 'active' : s.nav__link  // ← Используем 'active'
+              isActive ? `${s.nav__link} ${s.active}` : s.nav__link
             }
           >
             Junior Plus
