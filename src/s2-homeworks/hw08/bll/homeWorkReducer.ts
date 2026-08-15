9a604da0aa9893f7e41052c5f3c810a3cff3ce9c
AlinaRsy <al.alinarsy616@gmail.com>
2026-08-15 23:37:09 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,35 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      const newState = [...state];
+      if (action.payload === "up") {
+        return newState.sort((a, b) => {
+          if (a.name < b.name) return -1;
+          if (a.name > b.name) return 1;
+          return 0;
+        });
+      }
+      return newState.sort((a, b) => {
+        if (a.name > b.name) return -1;
+        if (a.name < b.name) return 1;
+        return 0;
+      });
     }
-}
+    case "check": {
+      return state.filter(u=>u.age>=18); // need to fix
+    }
+    default:
+      return state;
+  }
+};
